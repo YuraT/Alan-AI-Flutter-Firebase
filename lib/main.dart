ca7042c3e2ab1edfa7919b606647a19a4212150d
YuraT <ytatisch@gmail.com>
2020-04-13 19:09:02 -0700

    removed alan dependency and increased min. sdk ver

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:project1/models/user.dart';
 import 'package:project1/screens/wrapper.dart';
 import 'package:project1/services/auth.dart';
 import "package:provider/provider.dart";
-import 'package:alan_voice/alan_voice.dart';
+//import 'package:alan_voice/alan_voice.dart';
 
 void main() => runApp(MyApp());
 
