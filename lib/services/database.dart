055b8e679195785be825cedcbc71b7f6df8608e3
YuraT <ytatisch@gmail.com>
2020-05-24 14:54:22 -0700

    task puller fix

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -113,6 +113,7 @@ class DatabaseService {
       return TaskDataModel(
         title: doc.data["title"],
         description: doc.data["description"],
+        group: doc.data["group"],
         assigner: doc.data["assigner"],
         users: List.from(doc.data["users"]),
         deadline: /*DateTime.utc(2020, 4, 26, 14, 0),*/doc.data["deadline"].toDate(),
